f9fe144a9293608b47c746fe1f607f402d8536d1
Marco Favorito <marco.favorito@gmail.com>
2018-06-07 11:05:07 +0200

    update version number 0.1.3.post7

--- setup.py
+++ setup.py
@@ -41,6 +41,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/MarcoFavorito/flloat',
-    version='0.1.3post6',
+    version='0.1.3post7',
     zip_safe=False,
 )
