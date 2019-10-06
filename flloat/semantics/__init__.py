d5990672a04be6960bf2291f044444e88825062f
MarcoFavorito <marco.favorito@gmail.com>
2019-10-06 12:04:58 +0200

    fix docstrings for the flloat.semantics module.

--- flloat/semantics/__init__.py
+++ flloat/semantics/__init__.py
@@ -1,3 +1,6 @@
 # -*- coding: utf-8 -*-
+"""This module contains classes for the implementation of semantics for PL, LTLf and LDLf.
+
+In particular, it implements the propositional interpretation and a trace (i.e. a list of prop. int.)."""
 from .pl import PLInterpretation
 from .traces import FiniteTrace
