1be516eccb13f0699d70f18b05fbd9d07b5c52f7
MarcoFavorito <marco.favorito@gmail.com>
2019-10-10 17:54:10 +0200

    bump version and update HISTORY for release 0.2.3

--- flloat/__version__.py
+++ flloat/__version__.py
@@ -4,7 +4,7 @@
 __title__ = "flloat"
 __description__ = "A Python implementation of the FLLOAT library."
 __url__ = "https://github.com/marcofavorito/flloat.git"
-__version__ = "0.2.2"
+__version__ = "0.2.3"
 __author__ = "Marco Favorito"
 __author_email__ = "marco.favorito@gmail.com"
 __license__ = "Apache license"
